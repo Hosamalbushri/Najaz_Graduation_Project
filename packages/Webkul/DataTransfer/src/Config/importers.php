e5922953eda23d241aa0ae4f1016c8a4fafc8b01
Hosam Albushari <hosamalbushari@gmail.com>
2025-12-26 02:23:46 +0300

    updates

--- packages/Webkul/DataTransfer/src/Config/importers.php
+++ packages/Webkul/DataTransfer/src/Config/importers.php
@@ -1,51 +1,51 @@
 <?php
 
 return [
-    'products' => [
-        'title'    => 'data_transfer::app.importers.products.title',
-        'importer' => 'Webkul\DataTransfer\Helpers\Importers\Product\Importer',
-
-        'sample_paths' => [
-            'csv'  => 'data-transfer/samples/csv/products.csv',
-            'xls'  => 'data-transfer/samples/xls/products.xls',
-            'xlsx' => 'data-transfer/samples/xlsx/products.xlsx',
-            'xml'  => 'data-transfer/samples/xml/products.xml',
-        ],
-    ],
-
-    'customers' => [
-        'title'    => 'data_transfer::app.importers.customers.title',
-        'importer' => 'Webkul\DataTransfer\Helpers\Importers\Customer\Importer',
-
-        'sample_paths' => [
-            'csv'  => 'data-transfer/samples/csv/customers.csv',
-            'xls'  => 'data-transfer/samples/xls/customers.xls',
-            'xlsx' => 'data-transfer/samples/xlsx/customers.xlsx',
-            'xml'  => 'data-transfer/samples/xml/customers.xml',
-        ],
-    ],
-
-    'tax_rates' => [
-        'title'    => 'data_transfer::app.importers.tax-rates.title',
-        'importer' => 'Webkul\DataTransfer\Helpers\Importers\TaxRate\Importer',
-
-        'sample_paths' => [
-            'csv'  => 'data-transfer/samples/csv/tax-rates.csv',
-            'xls'  => 'data-transfer/samples/xls/tax-rates.xls',
-            'xlsx' => 'data-transfer/samples/xlsx/tax-rates.xlsx',
-            'xml'  => 'data-transfer/samples/xml/tax-rates.xml',
-        ],
-    ],
+//    'products' => [
+//        'title'    => 'data_transfer::app.importers.products.title',
+//        'importer' => 'Webkul\DataTransfer\Helpers\Importers\Product\Importer',
+//
+//        'sample_paths' => [
+//            'csv'  => 'data-transfer/samples/csv/products.csv',
+//            'xls'  => 'data-transfer/samples/xls/products.xls',
+//            'xlsx' => 'data-transfer/samples/xlsx/products.xlsx',
+//            'xml'  => 'data-transfer/samples/xml/products.xml',
+//        ],
+//    ],
+//
+//    'customers' => [
+//        'title'    => 'data_transfer::app.importers.customers.title',
+//        'importer' => 'Webkul\DataTransfer\Helpers\Importers\Customer\Importer',
+//
+//        'sample_paths' => [
+//            'csv'  => 'data-transfer/samples/csv/customers.csv',
+//            'xls'  => 'data-transfer/samples/xls/customers.xls',
+//            'xlsx' => 'data-transfer/samples/xlsx/customers.xlsx',
+//            'xml'  => 'data-transfer/samples/xml/customers.xml',
+//        ],
+//    ],
+//
+//    'tax_rates' => [
+//        'title'    => 'data_transfer::app.importers.tax-rates.title',
+//        'importer' => 'Webkul\DataTransfer\Helpers\Importers\TaxRate\Importer',
+//
+//        'sample_paths' => [
+//            'csv'  => 'data-transfer/samples/csv/tax-rates.csv',
+//            'xls'  => 'data-transfer/samples/xls/tax-rates.xls',
+//            'xlsx' => 'data-transfer/samples/xlsx/tax-rates.xlsx',
+//            'xml'  => 'data-transfer/samples/xml/tax-rates.xml',
+//        ],
+//    ],
 
     'services' => [
         'title'    => 'data_transfer::app.importers.services.title',
         'importer' => 'Najaz\Service\Helpers\Importers\Service\Importer',
 
         'sample_paths' => [
-            'csv'  => 'najaz-service/samples/csv/services.csv',
-            'xls'  => 'najaz-service/samples/xls/services.xls',
-            'xlsx' => 'najaz-service/samples/xlsx/services.xlsx',
-            'xml'  => 'najaz-service/samples/xml/services.xml',
+            'csv'  => 'data-transfer/samples/csv/services.csv',
+            'xls'  => 'data-transfer/samples/xls/services.xls',
+            'xlsx' => 'data-transfer/samples/xlsx/services.xlsx',
+            'xml'  => 'data-transfer/samples/xml/services.xml',
         ],
     ],
 
@@ -54,10 +54,10 @@ return [
         'importer' => 'Najaz\Citizen\Helpers\Importers\Citizen\Importer',
 
         'sample_paths' => [
-            'csv'  => 'najaz-citizen/samples/csv/citizens.csv',
-            'xls'  => 'najaz-citizen/samples/xls/citizens.xls',
-            'xlsx' => 'najaz-citizen/samples/xlsx/citizens.xlsx',
-            'xml'  => 'najaz-citizen/samples/xml/citizens.xml',
+            'csv'  => 'data-transfer/samples/csv/citizens.csv',
+            'xls'  => 'data-transfer/samples/xls/citizens.xls',
+            'xlsx' => 'data-transfer/samples/xlsx/citizens.xlsx',
+            'xml'  => 'data-transfer/samples/xml/citizens.xml',
         ],
     ],
 ];
